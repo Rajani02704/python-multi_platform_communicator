0d1c64a55846c4e6775cbdae06e1963a47ab253e
Rajani02704 <rajanichoudhary027@gmail.com>
2025-07-20 11:06:45 +0530

    Update multi_platform_communicator.py

--- multi_platform_communicator.py
+++ multi_platform_communicator.py
@@ -1,142 +1,107 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "26960928-5c63-4432-9ce4-a3a1d08c6731",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "\"\"\"\n",
-    "import smtplib ,ssl\n",
-    "from twilio.rest import Client\n",
-    "from linkedin_api import Linkedin\n",
-    "import tweepy\n",
-    "from fbchat import Client\n",
-    "from fbchat.models import Message\n",
-    "from instagrapi import Client\n",
-    "\n",
-    "\n",
-    "def send_email():\n",
-    "       s = smtplib.SMTP(\"smtp.gmail.com\", 587)\n",
-    "       s.starttls()\n",
-    "       s.login(sender_email, password)\n",
-    "       s.sendmail(sender_email, receiver_email, message.as_string())\n",
-    "\n",
-    "\n",
-    "def send_sms():\n",
-    "        client = Client(account_sid, auth_token)\n",
-    "    client.messages.create( to= \"+911234567890\", from= \"+91xxxxxxxxxx\", body= \"Hello!!!\")\n",
-    "\n",
-    "\n",
-    "def make_call():\n",
-    "    client = Client(account_sid, auth_token)\n",
-    "    client.calls.create( to= \"+911234567890\", from= \"+91xxxxxxxxxx\", url='http://demo.twilio.com/docs/voice.xml')\n",
-    "\n",
-    "\n",
-    "def post_linkedin():\n",
-    "        api = Linkedin('your_email', 'your_password')\n",
-    "        api.submit_post(\n",
-    "        'text='Hello Everyone!'\n",
-    "        )\n",
-    "\n",
-    "\n",
-    "def post_twitter():\n",
-    "       api_key = 'YOUR_API_KEY'\n",
-    "  api_key_secret = 'YOUR_API_KEY_SECRET'\n",
-    "  access_token = 'YOUR_ACCESS_TOKEN'\n",
-    "  access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'\n",
-    "  auth = tweepy.OAuth1UserHandler(api_key, api_key_secret, access_token, access_token_secret)\n",
-    "api = tweepy.API(auth)\n",
-    "tweet_text = \" Hello Everyone!\"\n",
-    "response = api.update_status(status=tweet_text)\n",
-    "\n",
-    "\n",
-    "def post_facebook():\n",
-    "    client = Client(username, password)\n",
-    "    client.send(Message(text=\"Hello!\"), thread_id=client.uid, thread_type=ThreadType.USER)\n",
-    "\n",
-    "\n",
-    "def post_instagram():\n",
-    "     cl = Client()\n",
-    "   cl.login(username, password)\n",
-    "   cl.send_message(\"Hello!!!\",caption)\n",
-    "\n",
-    "\n",
-    "def send_whatsapp():\n",
-    "    client = Client(account_sid, auth_token)\n",
-    "    message = client.messages.create(\n",
-    "    body='Hello!',\n",
-    "    from_='whatsapp:+141234567890', \n",
-    "    to='whatsapp:+91xxxxxxxxxx'      \n",
-    ")\n",
-    "\n",
-    "def main():\n",
-    "    menu =\n",
-    "    Choose a task:\n",
-    "    1. Send Email\n",
-    "    2. Send SMS\n",
-    "    3. Make a Phone Call\n",
-    "    4. Post on Linkedin \n",
-    "    5. Post on Twitter (X)\n",
-    "    6. Post on Facebook\n",
-    "    7. Post on Instagram\n",
-    "    8. Send WhatsApp Message\n",
-    "    9. Exit\n",
-    "\n",
-    "\n",
-    "\n",
-    "        while True:\n",
-    "        print(menu)\n",
-    "        choice = input(\"Enter choice (1-9): \")\n",
-    "        if choice == '1':\n",
-    "             send_email()\n",
-    "        elif choice == '2':\n",
-    "             send_sms()\n",
-    "        elif choice == '3':\n",
-    "            make_phone_call()\n",
-    "        elif choice == '4':\n",
-    "            post_linkedin()\n",
-    "        elif choice == '5':\n",
-    "            post_twitter()\n",
-    "        elif choice == '6':\n",
-    "            post_facebook()\n",
-    "        elif choice == '7':\n",
-    "            post_instagram()\n",
-    "        elif choice == '8':\n",
-    "            send_whatsapp()\n",
-    "        elif choice == '9':\n",
-    "            print(\"Exiting...\")\n",
-    "            break\n",
-    "        else:\n",
-    "            print(\"Invalid option. Try again.\")\n",
-    "\n",
-    "\n",
-    "    if __name__ == \"__main__\":\n",
-    "    main_menu()\n",
-    " \"\"\"  "
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+"""
+import smtplib ,ssl
+from twilio.rest import Client
+from linkedin_api import Linkedin
+import tweepy
+from fbchat import Client
+from fbchat.models import Message
+from instagrapi import Client
+
+
+def send_email():
+       s = smtplib.SMTP("smtp.gmail.com", 587)
+       s.starttls()
+       s.login(sender_email, password)
+       s.sendmail(sender_email, receiver_email, message.as_string())
+
+
+def send_sms():
+        client = Client(account_sid, auth_token)
+    client.messages.create( to= "+911234567890", from= "+91xxxxxxxxxx", body= "Hello!!!")
+
+
+def make_call():
+    client = Client(account_sid, auth_token)
+    client.calls.create( to= "+911234567890", from= "+91xxxxxxxxxx", url='http://demo.twilio.com/docs/voice.xml')
+
+
+def post_linkedin():
+        api = Linkedin('your_email', 'your_password')
+        api.submit_post(
+        'text='Hello Everyone!'
+        )
+
+
+def post_twitter():
+       api_key = 'YOUR_API_KEY'
+  api_key_secret = 'YOUR_API_KEY_SECRET'
+  access_token = 'YOUR_ACCESS_TOKEN'
+  access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+  auth = tweepy.OAuth1UserHandler(api_key, api_key_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+tweet_text = " Hello Everyone!"
+response = api.update_status(status=tweet_text)
+
+
+def post_facebook():
+    client = Client(username, password)
+    client.send(Message(text="Hello!"), thread_id=client.uid, thread_type=ThreadType.USER)
+
+
+def post_instagram():
+     cl = Client()
+   cl.login(username, password)
+   cl.send_message("Hello!!!",caption)
+
+
+def send_whatsapp():
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+    body='Hello!',
+    from_='whatsapp:+141234567890', 
+    to='whatsapp:+91xxxxxxxxxx'      
+)
+
+def main():
+    menu =
+    Choose a task:
+    1. Send Email
+    2. Send SMS
+    3. Make a Phone Call
+    4. Post on Linkedin 
+    5. Post on Twitter (X)
+    6. Post on Facebook
+    7. Post on Instagram
+    8. Send WhatsApp Message
+    9. Exit
+
+while True:
+        print(menu)
+        choice = input("Enter choice (1-9): ")
+        if choice == '1':
+             send_email()
+        elif choice == '2':
+             send_sms()
+        elif choice == '3':
+            make_phone_call()
+        elif choice == '4':
+            post_linkedin()
+        elif choice == '5':
+            post_twitter()
+        elif choice == '6':
+            post_facebook()
+        elif choice == '7':
+            post_instagram()
+        elif choice == '8':
+            send_whatsapp()
+        elif choice == '9':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid option. Try again.")
+
+
+    if __name__ == "__main__":
+    main_menu()
+ """  
+
